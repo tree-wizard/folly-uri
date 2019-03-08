ca6b71280a670dc3ca06afae74027ddc58776516
Xavier Cadena <xacadena@gmail.com>
2019-03-07 18:58:28 -0600

    Update fuzzer.cpp

--- fuzzer.cpp
+++ fuzzer.cpp
@@ -1,8 +1,18 @@
 #include <folly/Uri.h>
+#include <folly/String.h>
 
 extern "C" int LLVMFuzzerTestOneInput(const char *data, size_t size) {
+        folly::fbstring bin1(data, size);
 
-       folly::Uri u(data);
+        folly::Uri u(bin1);
+        u.scheme();
+        u.path();
+        u.username();
+        u.host();
+        u.authority();
+        u.password();
+        u.query();
+        u.fragment();
 
-       return 0;
+  return 0;
 }
