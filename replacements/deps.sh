8bcdc8a837eb1dc2d58e095abecb7bb37149ddd7
Xavier Cadena <xacadena@gmail.com>
2019-03-07 18:23:44 -0600

    Update and rename deps.sh to replacements/deps.sh

--- replacements/deps.sh
+++ replacements/deps.sh
@@ -63,24 +63,15 @@ fi
 
 
 # Get folly
-if [ ! -e folly/folly ]; then
-    echo "Cloning folly"
-    git clone https://github.com/facebook/folly
-fi
 cd folly
-git fetch
-git checkout "$folly_rev"
+
 
 # Build folly
 mkdir -p _build
 cd _build
-CXX=clang++-6.0 CXXFLAGS='-fPIC -g -fsanitize=fuzzer-no-link,undefined' cmake configure .. -DBUILD_SHARED_LIBS=ON
+CXX=clang++-7 CXXFLAGS='-ggdb -fsanitize=fuzzer-no-link' cmake configure .. -DFOLLY_ASAN_ENABLED=1 -DFOLLY_USE_SYMBOLIZER=1 -DBUILD_SHARED_LIBS=ON -DCMAKE_POSITION_INDEPENDENT_CODE=ON
 #cmake configure .. -DBUILD_SHARED_LIBS=ON -DCMAKE_POSITION_INDEPENDENT_CODE=ON
 make -j$JOBS
 make install
 
-if test $? -ne 0; then
-  echo "fatal: folly build failed"
-  exit -1
-fi
 cd ../..
